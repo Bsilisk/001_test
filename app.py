390f00a4cc77c28d5cf8959097e5f9ad96815bf6
Bsilisk <99400091+Bsilisk@users.noreply.github.com>
2022-06-08 21:56:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,11 @@ def main():
 			else:
 				st.warning("Incorrect Username/Password")
 
-
+	uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+	for uploaded_file in uploaded_files:
+	     bytes_data = uploaded_file.read()
+	     st.write("filename:", uploaded_file.name)
+	     st.write(bytes_data)
 
 if __name__ == '__main__':
 	main()
