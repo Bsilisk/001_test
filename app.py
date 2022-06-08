47c6fd0ba07961afca4bbc94f9045b79827a84ae
Bsilisk <99400091+Bsilisk@users.noreply.github.com>
2022-06-08 22:08:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,103 +1,97 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QFileDialog, QDialog
-from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtCore import Qt, QUrl, QEvent, QEventLoop
-from PyQt5.QtPrintSupport import QPrinter, QPrintDialog
-from PyQt5.QtGui import QPainter
-import sys
- 
-QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
- 
-class Form(QWidget):
- 
-    def __init__(self):
-        super().__init__()
- 
-        self.home = QUrl('https://google.com')
- 
-        hbox = QHBoxLayout()
-        self.btn_go = QPushButton('Move')
-        self.btn_pdf = QPushButton('PDF')
-        self.btn_prn = QPushButton('Print')
-        self.le_url = QLineEdit(self.home.toString())
-        hbox.addWidget(self.le_url)
-        hbox.addWidget(self.btn_go)
-        hbox.addWidget(self.btn_pdf)
-        hbox.addWidget(self.btn_prn)
-        hbox.setStretchFactor(self.le_url, 10)
- 
-        self.webview = QWebEngineView()
-        self.webview.load(self.home)
- 
-        vbox = QVBoxLayout(self)
-        vbox.addLayout(hbox)
-        vbox.addWidget(self.webview)
- 
-        self.setWindowTitle('Ocean Coding School')
-        self.setLayout(vbox)
- 
-        # signal
-        QApplication.instance().installEventFilter(self)
-        self.webview.urlChanged.connect(self.onUrlChanged)
-        self.btn_go.clicked.connect(self.onMove)
-        self.btn_pdf.clicked.connect(self.onPDF)
-        self.btn_prn.clicked.connect(self.onPrint)
- 
-    def eventFilter(self, obj, e):
-        if obj == self.le_url and e.type() == QEvent.KeyPress and e.key() == Qt.Key_Return:
-            self.onMove()
-            return True
-        return super().eventFilter(obj, e)
- 
-    def onUrlChanged(self, url):
-        self.le_url.setText(url.toString())
-        self.le_url.home(True)
- 
-    def onMove(self):
-        txt = self.le_url.text()
-        if txt.find('http')==-1 and txt.find('https')==-1:
-            txt = f'http://{txt}'
-            self.le_url.setText(txt)
- 
-        url = QUrl(txt)
-        self.webview.load(url)
- 
-    def onPDF(self):
-        page = self.webview.page()
-        path = QFileDialog.getSaveFileName(self, 'Save PDF', '', 'PDF File(*.pdf)')
-        if path[0]:
-            page.printToPdf(path[0])
- 
-    def onPrint(self):
-        printer = QPrinter()
-        printer.setResolution(QPrinter.HighResolution)
-        printer.setPageSize(QPrinter.A4)
-        dlg = QPrintDialog(printer, self.webview)
-        if dlg.exec_() != QDialog.Accepted:
-            return
- 
-        loop = QEventLoop()
-        result = False
- 
-        def printPreView(success):
-            nonlocal result
-            result = success
-            loop.quit()
- 
-        page = self.webview.page()
-        page.print(printer, printPreView)
-        loop.exec_()
- 
-        if not result:
-            qp = QPainter()
-            if qp.begin(printer):
-                font = qp.font()
-                font.setPixelSize(20)
-                qp.setFont(font)
-                qp.drawText(10, 25, "Could not generate print preview")
-                qp.end()
- 
+import streamlit as st
+import pandas as pd
+
+
+
+
+# Security
+#passlib,hashlib,bcrypt,scrypt
+import hashlib
+def make_hashes(password):
+	return hashlib.sha256(str.encode(password)).hexdigest()
+
+def check_hashes(password,hashed_text):
+	if make_hashes(password) == hashed_text:
+		return hashed_text
+	return False
+# DB Management
+import sqlite3 
+conn = sqlite3.connect('data.db')
+c = conn.cursor()
+# DB  Functions
+def create_usertable():
+	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
+
+
+def add_userdata(username,password):
+	c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
+	conn.commit()
+
+create_usertable()
+add_userdata("Adam",make_hashes("sm"))
+
+def login_user(username,password):
+	c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
+	data = c.fetchall()
+	return data
+
+
+def view_all_users():
+	c.execute('SELECT * FROM userstable')
+	data = c.fetchall()
+	return data
+
+
+
+def main():
+	"""Simple Login App"""
+	from PIL import Image
+	image = Image.open('sunrise.jpg')
+
+	st.image(image, caption='Sunrise by the mountains', width = 240)
+
+	st.title("Simple Login App")
+
+	menu = ["Home","Login"]
+	choice = st.sidebar.selectbox("Menu",menu)
+
+	if choice == "Home":
+		st.subheader("Home")
+
+	elif choice == "Login":
+		st.subheader("Login Section")
+
+		username = st.sidebar.text_input("User Name")
+		password = st.sidebar.text_input("Password",type='password')
+		if st.sidebar.checkbox("Login"):
+			# if password == '12345':
+			create_usertable()
+			hashed_pswd = make_hashes(password)
+
+			result = login_user(username,check_hashes(password,hashed_pswd))
+			if result:
+
+				st.success("Logged In as {}".format(username))
+
+				task = st.selectbox("Task",["Add Post","Analytics","Profiles"])
+				if task == "Add Post":
+					st.subheader("Add Your Post")
+
+				elif task == "Analytics":
+					st.subheader("Analytics")
+				elif task == "Profiles":
+					st.subheader("User Profiles")
+					user_result = view_all_users()
+					clean_db = pd.DataFrame(user_result,columns=["Username","Password"])
+					st.dataframe(clean_db)
+			else:
+				st.warning("Incorrect Username/Password")
+
+	uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+	for uploaded_file in uploaded_files:
+	     bytes_data = uploaded_file.read()
+	     st.write("filename:", uploaded_file.name)
+	     st.write(bytes_data)
+
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    w = Form()
-    w.show()
-    sys.exit(app.exec_())
+	main()
